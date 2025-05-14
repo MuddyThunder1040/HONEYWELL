d1e0250514fc1a0aec5815e65e79895676ccbf85
Vishnu3259 <vishnupardha3259@gmail.com>
2025-05-13 21:37:27 -0700

    First-Run

---
+++ terraform-jenkins-aws/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Jenkins-Infra"
+  }
+}
