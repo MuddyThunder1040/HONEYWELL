3534156447152d1cd6fa1479696c08ecf0105783
Vishnu3259 <vishnupardha3259@gmail.com>
2025-05-17 19:33:23 -0700

    Added outputs.tf

--- TF_basics/cat.tf
+++ TF_basics/cat.tf
@@ -1,6 +1,6 @@
 resource "local_file" "cat" {
   filename = "${path.module}/${var.file_name}"
-  content  = var.file_content
+  content  = "${random_pet.my_pet.id} ${var.file_content}"
 }
 
 resource "random_pet" "my_pet" {
@@ -8,3 +8,9 @@ resource "random_pet" "my_pet" {
   separator = var.separator
     length   = var.length
 }
+
+# resource "aws_instance" "webserver" {
+#     ami          = "ami-0c55b159cbfafe1f0"
+#     instance_type = "t2.micro"
+  
+# }
---
+++ TF_basics/outputs.tf
@@ -0,0 +1,4 @@
+output "name" {
+  value = random_pet.my_pet.id
+  
+}
